012e08c05e2071a2028d59fded6a0209b6dad0ed
Pavel Tatarskiy <pavel.tatarsky@gmail.com>
2020-10-25 20:55:18 +0300

    add get-webtor.sh

---
+++ scripts/get-webtor.sh
@@ -0,0 +1,16 @@
+sudo apt update
+sudo apt-get -y install snapd git curl
+sudo snap install microk8s --classic --channel=1.19/stable
+sudo microk8s status --wait-ready
+sudo microk8s kubectl get nodes
+sudo microk8s enable helm
+sudo microk8s enable dns
+sudo microk8s helm init --wait
+curl -L https://github.com/databus23/helm-diff/releases/download/v3.1.3/helm-diff-linux.tgz | sudo tar -C $(microk8s helm home)/plugins -xzv
+sudo microk8s helm repo add webtor https://charts.webtor.io
+sudo microk8s enable metrics-server
+curl -L 'https://github.com/roboll/helmfile/releases/download/v0.90.5/helmfile_linux_amd64' > helmfile
+chmod +x helmfile
+sudo mv ./helmfile /usr/local/bin/
+curl -L  'https://raw.githubusercontent.com/webtor-io/helm-charts/master/helmfile.yaml' > helmfile.yaml
+sudo helmfile --helm-binary=microk8s.helm apply
