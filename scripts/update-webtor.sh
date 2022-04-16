17cf6d7d326dbe75327b675a1e781be4f38af363
Pavel Tatarskiy <pavel.tatarsky@gmail.com>
2022-04-16 18:26:29 +0300

    update update script

--- scripts/update-webtor.sh
+++ scripts/update-webtor.sh
@@ -1,3 +1,4 @@
+sudo snap refresh microk8s --channel=1.22/stable
 curl -L  'https://raw.githubusercontent.com/webtor-io/helm-charts/master/helmfile.yaml' > helmfile.yaml
 helm repo up
 helmfile apply
